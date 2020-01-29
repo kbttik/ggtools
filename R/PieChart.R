470e8b1276be8def20dfff3a4d20a5e09d2ffdff
kbttik <taiki.j.mac@gmail.com>
2020-01-29 04:56:19 +0000

    add: define func for piechart #1

---
+++ R/PieChart.R
@@ -0,0 +1,16 @@
+#' 
+#' Caluculating the text posion in pie chart
+#' 
+#' @param y numeric pie chartで描く値
+#' 
+#' @return numeric textのポジション
+#' 
+make_piechart_text_position <- function(y){
+  y <- rev(y)
+  
+  # cumsumして自分の切れ目の端から半分(0.5)をポジションとする
+  y_cumsum <- cumsum(y) 
+  y_position <- y_cumsum - (y*.5)
+  
+  return(rev(y_position))
+}
